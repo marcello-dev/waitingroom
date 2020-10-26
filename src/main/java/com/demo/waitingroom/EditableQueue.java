ad9792e10bd1e05cf1aff13c9045a5481f13eca9
marcello-dev <monachesi.m@live.com>
2020-10-26 23:03:36 +0800

    format code

--- src/main/java/com/demo/waitingroom/EditableQueue.java
+++ src/main/java/com/demo/waitingroom/EditableQueue.java
@@ -5,9 +5,14 @@ import java.util.Optional;
 
 public interface EditableQueue<T> {
 	List<T> getAll();
+
 	T enqueue(T element);
+
 	Optional<T> dequeue();
+
 	void move(T element, int position);
+
 	int size();
+
 	void clear();
 }
